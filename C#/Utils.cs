1b3585673f2eda2441cd7874d84b6238e48bc10e
swordhack <131244293+wanghonghe@users.noreply.github.com>
2024-03-02 13:04:13 +0800

    Update Utils.cs

--- C#/Utils.cs
+++ C#/Utils.cs
@@ -1,4 +1,4 @@
-﻿using System;
+using System;
 using System.Collections.Generic;
 using System.DirectoryServices;
 using System.IO;
@@ -19,13 +19,13 @@ namespace SharpXDecrypt
         public static UserSID GetUserSID()
         {
             UserSID userSID;
-            Console.WriteLine("[*] Start GetUserSID....");
+            Console.WriteLine("[*] 开始获取当前用户SID....");
             WindowsIdentity current = WindowsIdentity.GetCurrent();
             userSID.Name = current.Name.ToString().Split('\\')[1];
             userSID.SID = current.User.ToString();
-            Console.WriteLine("  Username: " + userSID.Name);
-            Console.WriteLine("  userSID: " + userSID.SID);
-            Console.WriteLine("[*] GetUserSID Success !");
+            Console.WriteLine("  用户名: " + userSID.Name);
+            Console.WriteLine("  用户SID: " + userSID.SID);
+            Console.WriteLine("[** √] 获取当前用户SID成功 !");
             Console.WriteLine();
             return userSID;
         }
