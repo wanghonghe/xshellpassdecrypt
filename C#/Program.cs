41d2bdddb374482bc3243e0528d677382e956849
swordhack <131244293+wanghonghe@users.noreply.github.com>
2024-03-02 13:02:24 +0800

    Update Program.cs

--- C#/Program.cs
+++ C#/Program.cs
@@ -1,4 +1,4 @@
-﻿using Microsoft.Win32;
+using Microsoft.Win32;
 using System;
 using System.Collections.Generic;
 using System.DirectoryServices;
@@ -16,10 +16,8 @@ namespace SharpXDecrypt
         static void Main(string[] args)
         {
             Console.WriteLine();
-            Console.WriteLine("Xshell全版本凭证一键导出工具!(支持Xshell 7.0+版本)");
-            Console.WriteLine("Author: 0pen1");
-            Console.WriteLine("Github: https://github.com/JDArmy");
-            Console.WriteLine("[!] WARNING: For learning purposes only,please delete it within 24 hours after downloading!");
+            Console.WriteLine("Xshell全版本密码一键导出工具!(支持Xshell 7.0+版本)");
+
             Console.WriteLine();
             if (args.Length == 1)
             {
@@ -29,10 +27,7 @@ namespace SharpXDecrypt
             {
                 XClass.Decrypt();
             }
-            Console.WriteLine("[*] read done!");
-
-            // 等待用户按下任意键退出程序，当编译好好后可以直接双击运行而不是从cmd运行才能看到结果
-            Console.WriteLine("Press any key to exit...");
+            Console.WriteLine("[** √] 密码导出完成! 请按任意键退出! ");
             Console.ReadKey();
         }
 
